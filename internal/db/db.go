5808196b5f1c52e2488b72aeea1527f14ca99285
ferdie bergado <ferdiebergado@gmail.com>
2025-01-29 17:08:08 +0800

    Removed Querier interface

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,24 +1,7 @@
 package db
 
 import (
-	"context"
-	"database/sql"
 	"errors"
 )
 
-var ErrDuplicateUser = errors.New("user already exists")
 var ErrNullValue = errors.New("not null constraint violation")
-
-type Querier interface {
-	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
-}
-
-type repo struct {
-	db *sql.DB
-}
-
-var _ Querier = (*repo)(nil)
-
-func (r *repo) QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row {
-	return r.db.QueryRowContext(ctx, query, args...)
-}
