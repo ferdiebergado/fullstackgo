13202186cd5aaa72277032094afdced8650c2802
ferdie bergado <ferdiebergado@gmail.com>
2025-01-29 20:47:21 +0800

    Extracted json decoding to a function

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,3 +1,17 @@
 package api
 
+import (
+	"encoding/json"
+	"io"
+)
+
 const contentType = "application/json"
+
+func DecodeJSON(r io.Reader, dest any) error {
+	dec := json.NewDecoder(r)
+	dec.DisallowUnknownFields()
+	if err := dec.Decode(dest); err != nil {
+		return err
+	}
+	return nil
+}
